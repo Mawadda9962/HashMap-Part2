af26a454e7f12ea24159a5696cf4d2ec7018c3d9
Mawadda <moodalshukaili99@gmail.com>
2026-04-20 16:53:20 +0400

    Solving Task5

---
+++ src/Task5.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task5 {
+    public static void main(String[] args){
+        Map<String, Integer> FirstAndLastChar = new HashMap<>();
+
+        String[] word = {"salt", "tea", "soda", "toast"};
+
+
+        for(String words : word){
+            String key = words.substring(0,1);
+
+            if(words.contains())
+        }
+    }
+}
