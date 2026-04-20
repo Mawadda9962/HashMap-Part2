c5512e120e7b4132604f58f3a6386204a4d3ebd3
Mawadda <moodalshukaili99@gmail.com>
2026-04-20 15:20:47 +0400

    Solving Task2 part1

---
+++ src/Task2.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task2 {
+    public static void main(String[] args){
+        Map<String, Integer> uniqueWord = new HashMap<>();
+
+
+
+    }
+}
