8091773411c776f80119d8f89591ad91bf12b385
Mawadda <moodalshukaili99@gmail.com>
2026-04-20 19:42:02 +0400

    Solving Task7 part4

--- src/Task7.java
+++ src/Task7.java
@@ -11,7 +11,12 @@ public class Task7 {
         for(String words : word){
             if(wordAppears2OrMore.containsKey(words)){
                 wordAppears2OrMore.put(words, wordAppears2OrMore.get(words) + 1);
-
+            } else {
+                wordAppears2OrMore.put(words, 1);
+            }
         }
+
+
+    }
     }
 }
