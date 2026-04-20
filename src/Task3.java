60bb2bfca97c8cf4ff6586bec6317e8fa3ed282c
Mawadda <moodalshukaili99@gmail.com>
2026-04-20 15:38:59 +0400

    Solving Task3 Part6

--- src/Task3.java
+++ src/Task3.java
@@ -3,7 +3,12 @@ import java.util.Map;
 
 public class Task3 {
     public static void main(String[] args){
-        Map<String, Integer> FirstAndLastChar = new HashMap<>();
+        Map<String, String> FirstAndLastChar = new HashMap<>();
 
+        String[] words = {"code", "bug"};
+
+        for (String word: words){
+            FirstAndLastChar.put(word.charAt(0), word.)
+        }
     }
 }
