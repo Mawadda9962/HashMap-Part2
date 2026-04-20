d09f348905129d031dcb17accc0b36362b88ff95
Mawadda <moodalshukaili99@gmail.com>
2026-04-20 15:14:24 +0400

    Solving Task1

---
+++ src/Task1.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task1 {
+    public static void main(String[] args){
+        Map<String, Integer> uniqueWord = new HashMap<>();
+
+        String[] words = {"a", "b", "a", "b"};
+
+        for (String word : words){
+            uniqueWord.put(word,0);
+        }
+
+        System.out.println(uniqueWord);
+    }
+}
