a773ead2b273eb1212a84c8adbce0cd1935b2ffe
Mawadda <moodalshukaili99@gmail.com>
2026-04-20 19:41:28 +0400

    Solving Task7 part3

--- src/Task7.java
+++ src/Task7.java
@@ -4,10 +4,14 @@ import java.util.Map;
 public class Task7 {
     public static void main(String[] args){
 
-        Map<String, Integer> wordِِAppears2OrMore = new HashMap<>();
+        Map<String, Integer> wordAppears2OrMore = new HashMap<>();
 
         String[] word = {"a", "b", "a", "c", "b"};
 
         for(String words : word){
+            if(wordAppears2OrMore.containsKey(words)){
+                wordAppears2OrMore.put(words, wordAppears2OrMore.get(words) + 1);
+
+        }
     }
 }
