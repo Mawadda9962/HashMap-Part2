d2ad206b564dcd0467ae3cca00ea989117d6b8f5
Mawadda <moodalshukaili99@gmail.com>
2026-04-20 17:03:08 +0400

    Solving Task5 Group strings by their first character

--- src/Task5.java
+++ src/Task5.java
@@ -3,7 +3,7 @@ import java.util.Map;
 
 public class Task5 {
     public static void main(String[] args){
-        Map<String, String> FirstAndLastChar = new HashMap<>();
+        Map<String, String> groupString = new HashMap<>();
 
         String[] word = {"salt", "tea", "soda", "toast"};
 
@@ -11,13 +11,13 @@ public class Task5 {
 
             String key = words.substring(0, 1);
 
-            if (FirstAndLastChar.containsKey(key)) {
-                FirstAndLastChar.put(key, FirstAndLastChar.get(key) + words);
+            if (groupString.containsKey(key)) {
+                groupString.put(key, groupString.get(key) + words);
             } else {
-                FirstAndLastChar.put(key, words);
+                groupString.put(key, words);
             }
         }
 
-        System.out.println(FirstAndLastChar);
+        System.out.println(groupString);
     }
 }
