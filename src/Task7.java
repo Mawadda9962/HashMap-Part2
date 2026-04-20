4eb96825d2073cba699f4fb386803634d17d0745
Mawadda <moodalshukaili99@gmail.com>
2026-04-20 19:39:45 +0400

    Solving Task7 part1

---
+++ src/Task7.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task7 {
+    public static void main(String[] args){
+
+        Map<String, Integer> wordِِAppears2OrMore = new HashMap<>();
+
+        String[] word = {"a", "b", "a", "c", "b"};
+    }
+}
