eb876af15f2ecdc204b139df368436c9045a4bc2
Mawadda <moodalshukaili99@gmail.com>
2026-04-20 15:36:00 +0400

    Solving Task3 Part5

--- src/Task3.java
+++ src/Task3.java
@@ -1,3 +1,9 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Task3 {
-    public static void main(String[] args)
+    public static void main(String[] args){
+        Map<String, Integer> FirstAndLastChar = new HashMap<>();
+
+    }
 }
