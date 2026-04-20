ba06b673e2d53db10f0bd1dfd073ff2249170297
Mawadda <moodalshukaili99@gmail.com>
2026-04-20 15:23:49 +0400

    Solving Task2 part2

--- src/Task2.java
+++ src/Task2.java
@@ -3,7 +3,11 @@ import java.util.Map;
 
 public class Task2 {
     public static void main(String[] args){
-        Map<String, Integer> uniqueWord = new HashMap<>();
+        Map<String, Integer> uniqueWordAndLengthAsValue = new HashMap<>();
+
+        String[] word = {"a", "bb", "a", "bb"};
+
+        for
 
 
 
