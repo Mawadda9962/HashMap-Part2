20f2cc528b44954ef671eeedc7b756a15ce223af
Mawadda <moodalshukaili99@gmail.com>
2026-04-20 16:26:25 +0400

    Solving Task4 part1

---
+++ src/Task4.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task4 {
+    public static void main(String[] args){
+        Map<String, Integer> FirstAndLastChar = new HashMap<>();
+
+        String[] words = {"a", "b", "a", "c", "b"};
+
+        for(String word : words){
+
+            if(FirstAndLastChar.containsKey(word)){
+                FirstAndLastChar.put(word, FirstAndLastChar.get(word) + 1);
+            }else{
+                FirstAndLastChar.put(word,1);
+            }
+        }
+        sout
+    }
+}
