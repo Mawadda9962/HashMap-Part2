b8cc139ec35f42e5e32f97cb588dc46eb118cdab
Mawadda <moodalshukaili99@gmail.com>
2026-04-21 13:17:27 +0400

    Solving Task9 part 10

--- src/Task9.java
+++ src/Task9.java
@@ -3,7 +3,7 @@ import java.util.Map;
 
 public class Task9 {
     public static void main(String[] args){
-        String[] arr = {"ab", "ac"};
+        String[] arr = {};
         Map<Character, Integer> onlyOneSwap = new HashMap<>();
 
         for(int i = 0; i < arr.length; i++){
