34f60a841931b4c382e1efbc4d27bc8e81117098
Mawadda <moodalshukaili99@gmail.com>
2026-04-21 12:16:03 +0400

    Solving Task8 part5

--- src/Task8.java
+++ src/Task8.java
@@ -6,10 +6,15 @@ public class Task8 {
     public static void main(String[] args){
 
         String[] arr = {"ab", "ac"};
-
         Map<Character, Integer> characterIntegerHashMap = new HashMap<>();
 
-    }
+        for(int i = 0; i < arr.length; i++){
+            char ch = arr[i].charAt(0);
+
+        }
+
+
+        }
 
     }
 
