3412167ba8e21c4814fee66070aefcaa115a8e1d
Mawadda <moodalshukaili99@gmail.com>
2026-04-21 13:13:52 +0400

    Solving Task9 part 5

--- src/Task9.java
+++ src/Task9.java
@@ -4,14 +4,18 @@ import java.util.Map;
 public class Task9 {
     public static void main(String[] args){
         String[] arr = {"ab", "ac"};
-        Map<Character, Integer>  onlyOneSwap = new HashMap<>();
+        Map<Character, Integer> onlyOneSwap = new HashMap<>();
 
         for(int i = 0; i < arr.length; i++){
 
+            char ch = arr[i].charAt(0);
 
+            // إذا أول مرة نشوف الحرف
+            if(!onlyOneSwap.containsKey(ch)){
+                onlyOneSwap.put(ch, i);
+            }
 
+            // إذا موجود ولم يتم تعطيله
+            else if(onlyOneSwap.get(ch) != -1){
 
-
-
-        }
-}
+                int prevIndex = onlyOneSwap.get(ch);
