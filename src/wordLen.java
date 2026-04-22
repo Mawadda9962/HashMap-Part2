9ac1536674ccc955d24ff85ce3b60d4ab6a3f313
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-22 14:00:55 +0400

    Solving task2 rename class name

--- src/wordLen.java
+++ src/wordLen.java
@@ -1,7 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class Task2 {
+public class wordLen {
     public static void main(String[] args){
         Map<String, Integer> uniqueWordAndLengthAsValue = new HashMap<>();
 
